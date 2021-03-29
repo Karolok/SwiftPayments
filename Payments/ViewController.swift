f74c17273957854047d7e2352b6256a042813c63
Karol Bryzgiel <karol.bryzgiel@gmail.com>
2021-03-29 20:12:06 +0200

    Init

---
+++ Payments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Payments
+//
+//  Created by Karol Bryzgiel on 29/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
